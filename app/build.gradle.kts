908b8f3610f777c8726a288177deea8e2c8bf789
manoelsouzadev <manoel.souza280@gmail.com>
2024-11-14 17:53:53 -0300

    Adicionando estrutura de pastas, database e task dao

---
+++ app/build.gradle.kts
@@ -0,0 +1,83 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
+    id("kotlin-kapt")
+    id("kotlin-parcelize")
+    id("androidx.navigation.safeargs.kotlin")
+}
+
+android {
+    namespace = "br.edu.ifsp.scl.moviesmanager"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "br.edu.ifsp.scl.moviesmanager"
+        minSdk = 23
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+    buildFeatures {
+        compose = true
+        dataBinding = true
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
+
+    //Navigation
+    implementation("androidx.navigation:navigation-fragment-ktx:2.8.4")
+    implementation("androidx.navigation:navigation-ui:2.8.4")
+
+    // room
+    implementation("androidx.room:room-ktx:2.6.1")
+    kapt("androidx.room:room-compiler:2.6.1")
+    androidTestImplementation("androidx.room:room-testing:2.6.1")
+
+
+    // Lifecycle
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.7")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.8.7")
+    implementation("androidx.lifecycle:lifecycle-common-java8:2.8.7")
+
+    //Kotlin components
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:kotlin-android")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+}
---
+++ app/src/main/java/br/edu/ifsp/scl/moviesmanager/database/TaskDao.kt
@@ -0,0 +1,26 @@
+package br.edu.ifsp.scl.moviesmanager.database
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+
+@Dao
+interface TaskDao {
+
+    @Insert
+    suspend fun insert(taskEntry: TaskEntry)
+
+    @Delete
+    suspend fun delete(taskEntry: TaskEntry)
+
+    @Update
+    suspend fun update(taskEntry: TaskEntry)
+
+    @Query("DELETE FROM task_table")
+    suspend fun deleteAll()
+
+    @Query("SELECT * FROM task_table ORDER BY timestamp DESC")
+    suspend fun getAllTasks()
+}
